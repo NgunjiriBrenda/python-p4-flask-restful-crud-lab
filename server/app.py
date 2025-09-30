b73a0aee1f208870865392d2d2bd1fa23755929d
brenda njoki <ngunjiribrenda5@gmail.com>
2025-09-30 10:10:23 +0300

    Initial commit

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -47,6 +45,21 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        data = request.get_json()
+        
+        for attr in data:
+            setattr(plant, attr, data[attr])
+        
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
